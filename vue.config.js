4d4d28fa0e8cc0106a20b44de86211bc1bb0df59
ahmeddysf100 <ahmedysf100@gmail.com>
2024-09-30 00:00:35 +0300

    .

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/patient/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/<repository-name>/' : '/'
 })
